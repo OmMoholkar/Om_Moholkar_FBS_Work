b3be615de2cfbd9b0dc4732a531c20f12cefc69c
Om Moholkar <omoholkar30@gmail.com>
2025-11-03 20:40:34 +0530

    Assignment5

---
+++ C/Assignments/Assignment5/Q3.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main(){
+    int n=5;
+    for(int i=n; i>0; i--){
+        for(int j=1;j<=i;j++){
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+/*
+
+* * * * * 
+* * * *
+* * *
+* *
+*
+
+*/
---
+++ C/Assignments/Assignment5/Q7.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void main(){
+    int n=4;
+    int count=0;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+            count++;
+            printf("%d ", count);
+        }
+        printf("\n");
+    }
+}
+
+/*
+
+1 
+2 3
+4 5 6
+7 8 9 10
+
+*/
